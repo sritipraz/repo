9cac71299721ac1244caa3b073178a1a60b1557d
sritipraz <prajapatisriti.sp@gmail.com>
2023-04-29 15:41:41 -0500

    jwt

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")] // GET /api/users
-    public class UsersController : ControllerBase
+    [Authorize]
+     // GET /api/users
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
@@ -20,6 +21,7 @@ namespace API.Controllers
             _context = context;
         }
 
+    
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
@@ -27,7 +29,8 @@ namespace API.Controllers
 
             return users;
         }
-
+        
+        
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
